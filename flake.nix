0bec4b401ac5e3420575d831aedfb2648b487bb6
hbohlen <bohlenhayden@gmail.com>
2025-09-28 10:49:59 -0500

    feat(dev): add devShell + fmt helper

--- flake.nix
+++ flake.nix
@@ -1,12 +1,31 @@
 {
-  description = "hbohlen-systems: minimal desktop host";
+  description = "hbohlen-systems: minimal desktop host + devShell";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
   let
     system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
   in {
+    # ---- dev tools for working on this repo (no effect on your system) ----
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        git just ripgrep fd
+        alejandra nixfmt-rfc-style
+        direnv nix-direnv
+      ];
+      shellHook = ''
+        echo "Tip: run: echo 'use nix' > .envrc && direnv allow"
+      '';
+    };
+
+    packages.${system}.formatters = pkgs.writeShellScriptBin "fmt" ''
+      alejandra .
+      nixfmt --width 100 $(git ls-files '*.nix' || true)
+    '';
+
+    # ---- your existing NixOS host ----
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
       inherit system;
       modules = [
