22210b1e84b994682f8bc21096a977a85e53e057
Hayden Bohlen <bohlenhayden@gmail.com>
2025-09-28 21:25:19 -0500

    Add home-manager support to flake.nix
    Refactor flake.nix to include home-manager as an input and module.

--- flake.nix
+++ flake.nix
@@ -1,20 +1,21 @@
 {
   description = "hbohlen-systems: minimal desktop host + devShell";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, home-manager }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
   in {
-    # ---- dev tools for working on this repo (no effect on your system) ----
     devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        git just ripgrep fd
-        alejandra nixfmt-rfc-style
-        direnv nix-direnv
-      ];
+      packages = with pkgs; [ git just ripgrep fd alejandra nixfmt-rfc-style direnv nix-direnv ];
     };
 
     packages.${system}.formatters = pkgs.writeShellScriptBin "fmt" ''
@@ -22,12 +23,19 @@
       nixfmt --width 100 $(git ls-files '*.nix' || true)
     '';
 
-    # ---- your existing NixOS host ----
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
       inherit system;
       modules = [
         ./infrastructure/nixos/hosts/desktop/hardware-configuration.nix
         ./infrastructure/nixos/hosts/desktop/configuration.nix
+
+        # --- Home Manager as a NixOS module ---
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.hbohlen = import ./infrastructure/home/hbohlen/home.nix;
+        }
       ];
     };
   };
