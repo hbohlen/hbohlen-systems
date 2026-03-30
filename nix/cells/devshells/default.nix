0c68ad02044482a3856f86a52c71d19bb92801a3
User <user@example.com>
2026-03-30 07:59:06 +0000

    feat: add minimal starship configuration

--- nix/cells/devshells/default.nix
+++ nix/cells/devshells/default.nix
@@ -43,6 +43,9 @@
           echo "Entering hbohlen-systems devShell..."
           export SHELL=${pkgs.fish}/bin/fish
 
+          # Set starship config
+          export STARSHIP_CONFIG=${./config/starship.toml}
+
           # Start fish if not already in fish
           if [[ -z "$FISH_VERSION" ]]; then
             exec ${pkgs.fish}/bin/fish
