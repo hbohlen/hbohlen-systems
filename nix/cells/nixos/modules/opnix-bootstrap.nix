08535ff4bf7a7ce8d40da92c1e54f0f0d013349d
User <user@example.com>
2026-03-31 22:10:53 +0000

    feat(nixos): add Tailscale with hardcoded authkey

--- nix/cells/nixos/modules/opnix-bootstrap.nix
+++ nix/cells/nixos/modules/opnix-bootstrap.nix
@@ -1,6 +1,22 @@
 # Tailscale + opnix bootstrap module
 { config, pkgs, ... }:
 
+let
+  # From 1Password: op read op://hbohlen-systems/tailscale/authKey
+  tailscaleAuthKey = "tskey-auth-k8dtzB3tS821CNTRL-yxmcjnUZmd3yXxLfZbN7d3ehY84oYfTe";
+  tailscaleTags = "tag:server,tag:prod";
+in
 {
-  # Placeholder - will be filled in subsequent tasks
+  services.tailscale = {
+    enable = true;
+    openFirewall = true;
+    useRoutingFeatures = "server";
+    extraUpFlags = [
+      "--ssh"
+      "--advertise-tags=${tailscaleTags}"
+      "--authkey=${tailscaleAuthKey}"
+    ];
+  };
+
+  systemd.services.tailscale.wantedBy = [ "multi-user.target" ];
 }
