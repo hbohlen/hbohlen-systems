5af2c1df8db23b72b35f35761951f01e3d65855a
User <user@example.com>
2026-04-01 20:41:13 +0000

    feat: add user module combining NixOS and Home Manager config

---
+++ modules/user.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  # NixOS: user creation
+  users.users.hbohlen = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    shell = pkgs.fish;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICP6MnCIoDGFnx42wAmVgoNxaHxEtRnOF10d3q/xOIZG hbohlen@hetzner"
+    ];
+  };
+
+  programs.fish.enable = true;
+
+  security.sudo.wheelNeedsPassword = false;
+
+  # Home Manager: user-level config
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.hbohlen = { pkgs, ... }: {
+      home.stateVersion = "24.11";
+
+      programs.ssh = {
+        enable = true;
+        extraConfig = ''
+          IdentityAgent ~/.ssh/agent.sock
+        '';
+      };
+
+      home.sessionVariables = {
+        OP_SERVICE_ACCOUNT_TOKEN_FILE = "/etc/opnix-token";
+      };
+    };
+  };
+}
---
+++ tests/evaluation/test-base.nix
@@ -0,0 +1,35 @@
+{ inputs, ... }:
+
+{
+  perSystem = { pkgs, lib, ... }:
+    let
+      homeManagerModule = inputs.home-manager.nixosModules.home-manager;
+
+      minimalEvalConfig = {
+        fileSystems."/" = {
+          device = "/dev/sda1";
+          fsType = "ext4";
+        };
+        boot.loader.grub.devices = [ "/dev/sda" ];
+      };
+    in
+    {
+      nix-unit.tests.testBaseEvaluates = {
+        expr =
+          let
+            result = pkgs.nixos [ ../../modules/base.nix minimalEvalConfig ];
+          in
+          result.config.networking.hostName != null;
+        expected = true;
+      };
+
+      nix-unit.tests.testUserEvaluates = {
+        expr =
+          let
+            result = pkgs.nixos [ ../../modules/base.nix ../../modules/user.nix minimalEvalConfig homeManagerModule ];
+          in
+          result.config.users.users.hbohlen.isNormalUser;
+        expected = true;
+      };
+    };
+}
